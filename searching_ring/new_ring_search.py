37772606dcbe26b0f8eb80b55b091f4b388dd44f
Charles RUBIRIGI <charles.rubirigi@unamur.be>
2025-04-25 15:18:14 +0200

    **refactor** split primitive ring search in three functions

--- searching_ring/new_ring_search.py
+++ searching_ring/new_ring_search.py
@@ -3,8 +3,22 @@ import time
 
 from itertools import combinations
 
-def find_all_shortest_paths(graph, starting_node:int, final_node:int, sortest_path_length:dict):
-    #PREMIERE TENTATIVE
+def find_all_shortest_paths(graph:nx.Graph, starting_node:int, final_node:int, sortest_path_length:dict) -> list[list]:
+    '''
+    Find all shortest paths between two nodes in a graph knowing the length of those paths. 
+    This is much faster than when the length is not known.
+    -------------------------------------------------------------------------------------------------------
+    Inputs: 
+    
+            graph: nteworkX graph
+            starting node: starting node index
+            final node: target node index
+            shortest_path_length: dict[dict[int]] containing shortest paths length for each node pairs
+
+    Outputs:
+            A list of every shortest paths between starting and target node
+
+    '''
     queue = [[starting_node]]
     path = []
     while len(queue) > 0:
@@ -18,68 +32,87 @@ def find_all_shortest_paths(graph, starting_node:int, final_node:int, sortest_pa
             queue.remove(nodcrt)
     return path
 
-def primitive_ring_search(graph:nx.Graph, node_list:list=[0], max_ring_size:int=20):
+def find_prime_mid_nodes(src_node:int, shortest_dist_dict:dict[dict[list]], max_ring_size:int, graph:nx.Graph):
+    shortest_paths = shortest_dist_dict[src_node]
+    prime_mid_node = {x:[] for x in range(2, max_ring_size + 1)}
+    # start_time = time.time()
+    for node, length in shortest_paths.items():
+        if length <= max_ring_size//2 and length > 1:
+            neighb_dist_list = [shortest_paths[neighbors] for neighbors in graph.neighbors(node)]
+            if neighb_dist_list.count(length - 1) > 1:
+                prime_mid_node[2*length].append(node)
+            neighb_dist_dict = {neighbors:shortest_paths[neighbors] for neighbors in graph.neighbors(node)}
+            for n, path in neighb_dist_dict.items():
+                if path == length and sorted([n, node]) not in prime_mid_node[2*length + 1]:
+                    prime_mid_node[2*length + 1].append(sorted([n, node]))
+    return prime_mid_node
+
+def form_rings(prime_mid_node:dict, src_node:int, shortest_dist_dict:dict[dict[list]], max_ring_size:int, graph:nx.Graph):
+    future_rings  = {x:[] for x in range(2, max_ring_size + 1)}
+    for ring_size, list_nodes in prime_mid_node.items():
+        if ring_size%2 == 0:
+            for nodes in list_nodes:
+                for poss_ring in combinations(find_all_shortest_paths(graph, nodes, src_node, shortest_dist_dict), 2):
+                    ring_a, ring_b = poss_ring
+                    if len(set(ring_a[1:-1]) & set(ring_b[1:-1])) == 0:
+                        future_rings[ring_size].append([ring_a, ring_b])
+        elif ring_size%2 == 1:
+            for lnked_nodes in list_nodes:
+                rings_a = find_all_shortest_paths(graph, lnked_nodes[0], src_node, shortest_dist_dict)
+                rings_b = find_all_shortest_paths(graph, lnked_nodes[1], src_node, shortest_dist_dict)
+                for ring_a in rings_a:
+                    for ring_b in rings_b:
+                        if len(set(ring_a[:-1]) & set(ring_b[:-1])) == 0:
+                            future_rings[ring_size].append([ring_a, ring_b])
+    return future_rings
+
+def exclude_non_primitive_rings(potential_rings:dict, max_ring_size:int, shortest_dist_dict:dict[dict[list]]):
+    primitive_ring = {x:[] for x in range(2, max_ring_size + 1)}
+    for ring_size, list_rings in potential_rings.items():
+        for rings in list_rings:
+            ring_a, ring_b = rings
+            shortcut = 0
+            for i in range(1, len(ring_a) - 1):
+                if shortest_dist_dict[ring_a[i]][ring_b[::-1][i]] !=  ring_size//2:
+                    shortcut += 1
+                    break
+            if shortcut == 0:
+                if ring_size%2 == 0:
+                    primitive_ring[ring_size].append(ring_a[1:-1]+ring_b[::-1])
+                    # primitive_ring_count[ring_size] += 1
+                elif ring_size%2 == 1:
+                    primitive_ring[ring_size].append(ring_a[:-1]+ring_b[::-1])
+                    # primitive_ring_count[ring_size] += 1
+    return primitive_ring
+
+def find_primitive_ring(all_path_length:dict[dict[list]], graph:nx.Graph, src_node:int, max_dist:int):
+    nodes = find_prime_mid_nodes(src_node=src_node, shortest_dist_dict=all_path_length, max_ring_size=max_dist, graph=graph)
+
+    rings = form_rings(prime_mid_node=nodes,src_node=src_node,shortest_dist_dict=all_path_length,max_ring_size=max_dist, graph=graph)
+
+    primitive = exclude_non_primitive_rings(potential_rings=rings, max_ring_size=max_dist, shortest_dist_dict=all_path_length)
+    return primitive
+
+def primitive_ring_search(graph:nx.Graph, node_list:list=None, max_ring_size:int=20, verbose=True) -> dict:
+    if node_list is None:
+        node_list = graph.nodes
+        print("Perform ring search on the entire graph.")
     start_time = time.time()
     furthest_search = max_ring_size//2 + 1
     start_path = time.time()
-    all_shortest_path_length = dict(nx.all_pairs_shortest_path_length(graph, cutoff=furthest_search))
+    all_paths = dict(nx.all_pairs_shortest_path_length(graph, cutoff=furthest_search))
     end_path = time.time()
     print(f"Computing all shortest path lengths took {end_path -start_path} seconds")
-    primitive_ring_count = {x:0 for x in range(4, max_ring_size + 1)}
+    all_rings = {x:dict() for x in node_list}
     for src_node in node_list:
-        prime_mid_node = {x:[] for x in range(4, max_ring_size + 1)}
-        future_rings  = {x:[] for x in range(4, max_ring_size + 1)}
-        primitive_ring =  {x:[] for x in range(4, max_ring_size + 1)}
-
-        shortest_paths = all_shortest_path_length[src_node]
-        # start_time = time.time()
-        for node, length in shortest_paths.items():
-            if length <= max_ring_size//2 and length > 1:
-                neighb_dist_list = [shortest_paths[neighbors] for neighbors in graph.neighbors(node)]
-                if neighb_dist_list.count(length - 1) > 1:
-                    prime_mid_node[2*length].append(node)
-                neighb_dist_dict = {neighbors:shortest_paths[neighbors] for neighbors in graph.neighbors(node)}
-                for n, path in neighb_dist_dict.items():
-                    if path == length and sorted([n, node]) not in prime_mid_node[2*length + 1]:
-                        prime_mid_node[2*length + 1].append(sorted([n, node]))
-        # end_time = time.time()
-        # print(f"It took {end_time - start_time} seconds to find prime mid nodes")
-        # start_time = time.time()
-        for ring_size, list_nodes in prime_mid_node.items():
-            if ring_size%2 == 0:
-                for nodes in list_nodes:
-                    for poss_ring in combinations(find_all_shortest_paths(graph, nodes, src_node, all_shortest_path_length), 2):
-                        ring_a, ring_b = poss_ring
-                        if len(set(ring_a[1:-1]) & set(ring_b[1:-1])) == 0:
-                            future_rings[ring_size].append([ring_a, ring_b])
-            elif ring_size%2 == 1:
-                for lnked_nodes in list_nodes:
-                    rings_a = find_all_shortest_paths(graph, lnked_nodes[0], src_node, all_shortest_path_length)
-                    rings_b = find_all_shortest_paths(graph, lnked_nodes[1], src_node, all_shortest_path_length)
-                    for ring_a in rings_a:
-                        for ring_b in rings_b:
-                            if len(set(ring_a[:-1]) & set(ring_b[:-1])) == 0:
-                                future_rings[ring_size].append([ring_a, ring_b])
-        # end_time = time.time()
-        # print(f"It took {end_time - start_time} seconds to form rings")
-        # start_time = time.time()
-        for ring_size, list_rings in future_rings.items():
-            for rings in list_rings:
-                ring_a, ring_b = rings
-                shortcut = 0
-                for i in range(1, len(ring_a) - 1):
-                    if all_shortest_path_length[ring_a[i]][ring_b[::-1][i]] !=  ring_size//2:
-                        shortcut += 1
-                        break
-                if shortcut == 0:
-                    if ring_size%2 == 0:
-                        primitive_ring[ring_size].append(ring_a[1:-1]+ring_b[::-1])
-                        primitive_ring_count[ring_size] += 1
-                    elif ring_size%2 == 1:
-                        primitive_ring[ring_size].append(ring_a[:-1]+ring_b[::-1])
-                        primitive_ring_count[ring_size] += 1
-        # end_time = time.time()
-        # print(f"It took {end_time - start_time} seconds to exclude non-primitive rings")
+        all_rings[src_node] = find_primitive_ring(src_node=src_node, all_path_length=all_paths, graph=graph, max_dist=max_ring_size)
     end_time = time.time()
     print(f"Computing all primitive rings up to {max_ring_size}-rings on {len(node_list)} atoms took {end_time - start_time} seconds")
-    print(primitive_ring_count)
+    # print(primitive_ring_count)
+    if verbose is True:
+        primitive_ring_count = {x:0 for x in range(2, max_ring_size + 1)}
+        for src_node in node_list:
+            for size, rings in all_rings[src_node].items():
+                primitive_ring_count[size] += len(rings)
+        print(primitive_ring_count)
+    return all_rings
